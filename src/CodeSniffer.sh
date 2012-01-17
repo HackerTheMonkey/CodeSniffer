a47db3b30b397c0257aa8069571fb7cfd377fda9
hasanein <hasanein.ali@gmail.com>
2012-01-17 03:03:24 +0000

    Adding shell script coding template

---
+++ src/CodeSniffer.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# SCRIPT: CodeSniffer.sh
+# AUTHOR: Hasanein Khafaji
+# DATE: 01/17/2012
+# REV: 1.0.0
+# PLATFORM: Linux & UNIX
+#
+# PURPOSE: The CodeSniffer tool is a shell script that searchs through SQL source code files looking 
+# for matching patterns using regular expressions and help gathering information about procedures and 
+# functions call stack and dependacies.
+#      
+# set -x # Uncomment to debug this script
+#
+# set -n # Uncomment to check script syntax
+#        # without aby execution. Do not forget
+#        # to put the comment back in or the
+#        # script will never execute.
+#
+##########################################
+# DEFINE FILES AND VARIABLES HERE
+##########################################
+
+
+##########################################
+# DEFINE FUNCTIONS HERE
+##########################################
+
+
+##########################################
+# BEGINNING OF MAIN
+##########################################
+
+
+
+##########################################
+# END OF CodeSniffer.sh SCRIPT
+##########################################
